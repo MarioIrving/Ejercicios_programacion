51768fe6950eaceaa9cac5e337ae89e9bf2dd784
MoralesMario <marioirving.morales@gmail.com>
2018-03-26 17:56:18 -0600

    finisc

--- Angular/calc-app/app.js
+++ Angular/calc-app/app.js
@@ -3,12 +3,26 @@ var app =  angular.module("calcApp", []);
 	app.controller("calcCtrl",calcCtrl );
 
 	function calcCtrl(){
-
+		this.resultValue=0;
 		this.ButtonClicked=function(button){
-
 			this.selectedOperation = button;
 		}
 
-		console.log("Mi primer controlador");
+		this.computeResult = function (){
+
+				var number1 = parseFloat(this.input1);
+				var number2 = parseFloat(this.input2);
+
+				if(this.selectedOperation === '+'){
+					this.resultValue = number1 + number2;
+				} else if(this.selectedOperation === '-'){
+					this.resultValue = number1 - number2;
+				} else if (this.selectedOperation === '*') {
+					this.resultValue = number1 * number2;
+				} else if (this.selectedOperation === '/') {
+					this.resultValue = number1 / number2;
+				}
+
+		}
 
 	}
